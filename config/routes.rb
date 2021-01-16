314b606053bb45bd6a354036355f13b9b0677c27
Bill Mei <billmei@users.noreply.github.com>
2021-01-16 14:48:02 -0500

    Sources controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'sources/index'
+  get '/submit', to: 'sources#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
